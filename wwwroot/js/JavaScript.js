5533076a15c8c95106f80923fde6390a91487d70
Charlotte Ranemo <info@charlotteranemo.se>
2021-01-25 09:46:14 +0100

    Uploading files

---
+++ wwwroot/js/JavaScript.js
@@ -0,0 +1,10 @@
+﻿var isOpen = false;
+function openMenu() {
+    if (!isOpen) {
+        document.getElementById("mainNav").style.display = "block";
+        isOpen = true;
+    } else {
+        document.getElementById("mainNav").style.display = "none";
+        isOpen = false;
+    }
+}
